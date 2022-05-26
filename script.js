093445bddc2e9c164550a02340ae7c1b9ffb2f38
CristinaGrosser <cmgrosser@comcast.net>
2022-05-26 14:34:20 -0700

    New styling for skills section

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ imgCont.addEventListener('mouseout', () => {
   img.setAttribute('src', './assets/Optimized-IMG_1863.jpg');
   img.classList.remove('hikerMe');
 })
+
+// revealing text on hover of icons
+const iconLI = document.querySelectorAll('.iconLI');
+
+iconLI.forEach((listItem) => {
+  listItem.addEventListener('mouseover', () => {
+    listItem.classList.add('flip');
+  })
+  listItem.addEventListener('mouseout', () => {
+    listItem.classList.remove('flip');
+  })
+})
