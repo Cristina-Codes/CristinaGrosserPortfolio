053e51ede41f190980511e9e88144ae8a3378172
CristinaGrosser <cmgrosser@comcast.net>
2022-07-25 12:16:46 -0700

    image updates

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const imgCont = document.querySelector('.imgContainer');
-const img = document.querySelector('#me');
+// const imgCont = document.querySelector('.imgContainer');
+// const img = document.querySelector('#me');
 
-imgCont.addEventListener('mouseover', () => {
-  img.setAttribute('src', './assets/Optimized-IMG_9269.jpeg');
-  img.classList.add('hikerMe');
-})
+// imgCont.addEventListener('mouseover', () => {
+//   img.setAttribute('src', './assets/Optimized-IMG_9269.jpeg');
+//   img.classList.add('hikerMe');
+// })
 
-imgCont.addEventListener('mouseout', () => {
-  img.setAttribute('src', './assets/Optimized-IMG_1863.jpg');
-  img.classList.remove('hikerMe');
-})
+// imgCont.addEventListener('mouseout', () => {
+//   img.setAttribute('src', './assets/Optimized-IMG_1863.jpg');
+//   img.classList.remove('hikerMe');
+// })
 
 // revealing text on hover of icons
-const iconLI = document.querySelectorAll('.iconLI');
+const iconLI = document.querySelectorAll(".iconLI");
 
 iconLI.forEach((listItem) => {
-  listItem.addEventListener('mouseover', () => {
-    listItem.classList.add('flip');
-  })
-  listItem.addEventListener('mouseout', () => {
-    listItem.classList.remove('flip');
-  })
-})
+  listItem.addEventListener("mouseover", () => {
+    listItem.classList.add("flip");
+  });
+  listItem.addEventListener("mouseout", () => {
+    listItem.classList.remove("flip");
+  });
+});
