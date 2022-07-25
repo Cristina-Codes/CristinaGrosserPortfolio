18b4122193235ea825f28a8b672de079248e805b
CristinaGrosser <cmgrosser@comcast.net>
2022-07-25 12:21:58 -0700

    code cleanup, skills updating

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// const imgCont = document.querySelector('.imgContainer');
-// const img = document.querySelector('#me');
-
-// imgCont.addEventListener('mouseover', () => {
-//   img.setAttribute('src', './assets/Optimized-IMG_9269.jpeg');
-//   img.classList.add('hikerMe');
-// })
-
-// imgCont.addEventListener('mouseout', () => {
-//   img.setAttribute('src', './assets/Optimized-IMG_1863.jpg');
-//   img.classList.remove('hikerMe');
-// })
-
 // revealing text on hover of icons
 const iconLI = document.querySelectorAll(".iconLI");
 
